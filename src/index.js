6448801c4fda80db642c11491d08bc5fcdb14445
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-16 02:41:34 -0500

    Set up store and reducers/actions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
+import store from './store';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/users.js
@@ -0,0 +1,6 @@
+export const (state = [], action) => {
+  switch (action.type) {
+    default:
+    return state
+  }
+}
