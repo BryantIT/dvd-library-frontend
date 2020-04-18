d192fd1139c18e6e83ddc67123da5f3598c4fda2
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-18 03:40:28 -0500

    loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { EatLoading } from 'react-loading';
 
 const Loading = () => {
   return (
     <div>
       <h1>Your Library is currently loading....</h1>
+      <EatLoading />
     </div>
 
   )
