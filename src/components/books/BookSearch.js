12613233e1ce3c4a6447dd47a18b6f38ef109302
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-24 00:13:29 -0500

    set up bar and handler

--- src/components/books/BookSearch.js
+++ src/components/books/BookSearch.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const BookSearch = ({ currentUser }) => {
+const BookSearch = props => {
+
+  const handleChange = event => {
+    const { name, value } = event.target
+  }
+
+  const bookCards =
+
+    props.books.sort((a, b) => a.attributes.title.localeCompare(b.attributes.title)).map(t => (
+      <div key={t.id} className="w3-quarter w3-margin-bottom">
+        <div className="box gallery">
+          <div className="card">
+            <div className="background">
+              <div className="info">
+                <Link className="overlay" to={`/books/${t.id}`}></Link>
+                <h2>{t.attributes.title}</h2>
+                <p><small>by: </small>{t.attributes.author}</p>
+                <div className="foot">
+                  <i className="line"></i>
+                  <p>{t.attributes.description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>))
   return (
-    currentUser ?
-    <div className="w3-margin-bottom">
-        <input className='search-input' type="text" placeHolder="Search"/>
-    </div> : ""
+    <div className="w3-row-padding w3-container w3-padding-32 w3-center">
+    <div className="search-container">
+        <input type="text" className="search-input" onChange={handleChange}></input>
+    </div>
+        {bookCards}
+    </div>
   )
+
 }
 
-const mapStateToProps = ({ currentUser }) => {
+const mapStateToProps = state => {
   return {
-    currentUser
+    books: state.userBooks
   }
 }
 
